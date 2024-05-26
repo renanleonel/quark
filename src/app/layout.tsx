10cd268b2ccaa03ddfac16a1c33f18ea07a46dae
renanleonel <renanleonelpro@gmail.com>
2024-05-26 12:20:04 -0300

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,22 @@ export default function RootLayout({
 }) {
     return (
         <html lang='en' className={GeistSans.className}>
+            <head>
+                <link rel='icon' href='/favicon.ico' sizes='any' />
+                <link rel='manifest' href='/manifest.json' />
+                <meta
+                    name='apple-mobile-web-app-status-bar-style'
+                    content='black-translucent'
+                />
+                <meta
+                    name='viewport'
+                    content='initial-scale=1, viewport-fit=cover'
+                />
+                <meta property='og:image' content='<generated>' />
+                <meta property='og:image:type' content='<generated>' />
+                <meta property='og:image:width' content='<generated>' />
+                <meta property='og:image:height' content='<generated>' />
+            </head>
             <body className='h-full min-h-screen bg-background'>
                 <Providers>{children}</Providers>
             </body>
